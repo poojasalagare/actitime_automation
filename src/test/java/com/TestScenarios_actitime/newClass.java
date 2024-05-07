0df6b7e35a475621effee84806727f20225815a1
poojasalagare <poojasalagare19@gmail.com>
2024-05-07 14:20:45 +0530

    new class added

---
+++ src/test/java/com/TestScenarios_actitime/newClass.java
@@ -0,0 +1,14 @@
+package com.TestScenarios_actitime;
+
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.testng.annotations.Test;
+
+public class newClass {
+
+
+    @Test
+    public void m1()
+    {
+        System.out.println("hello");
+    }
+}
